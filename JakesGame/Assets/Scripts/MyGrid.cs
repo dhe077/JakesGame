ac0e6f060fd0cac94b809b9837ca8f0ef23a13e8
dhe077 <dhe077@aucklanduni.ac.nz>
2021-08-12 00:46:13 +1200

    Grid Script?

---
+++ JakesGame/Assets/Scripts/MyGrid.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGrid {
+
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+    public MyGrid(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        gridArray = new int[width, height];
+
+        for (int i = 0; i < gridArray.GetLength(0); i++) {
+            for (int j = 0; j < gridArray.GetLength(1); j++) {
+                Debug.Log(i + ", " + j);
+            }
+        }
+    }
+}
---
+++ JakesGame/Assets/Scripts/Testing.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        MyGrid grid = new MyGrid(4, 2);
+    }
+}
